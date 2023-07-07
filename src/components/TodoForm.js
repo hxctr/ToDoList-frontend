222f81b8a32465a5004c6be83d51d6afdedcb523
hectorponsoy <hectorjosue65@gmail.com>
2023-07-06 21:46:23 -0600

    POST request connected with todo-api

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -3,6 +3,8 @@ import { BsArrowDown, BsPlusCircleFill } from "react-icons/bs";
 import { RiCheckboxCircleLine } from "react-icons/ri";
 
 function TodoForm(props) {
+
+  //esto es para los inputs principales
   const [input, setInput] = useState(props.edit ? props.edit.value : "");
   const [showDescription, setShowDescription] = useState(false);
   const [description, setDescription] = useState(
@@ -27,13 +29,42 @@ function TodoForm(props) {
   const handleSubmit = (e) => {
     e.preventDefault();
 
+    //este si es para los todos list
     props.onSubmit({
       id: Math.floor(Math.random() * 10000),
       text: input,
       description,
       isDone: false,
-      showDescription: false,
+      showDescription: true,
     });
+
+
+    //este objeto es para el fetch con el POST
+    const todoData = {
+      title: input,
+      description: description
+    };
+
+    //--
+    // peticion POST para aniadir un nuevo todo
+    fetch('https://todo-api-h8ov.onrender.com/api', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(todoData)
+    })
+      .then(response => {
+        if (response.ok) {
+          console.log("Datos enviados exitosamente");
+        } else {
+          throw new Error("Error en la petición");
+        }
+      })
+      .catch(error => {
+        console.error("Error al enviar los datos:", error);
+      });
+    //--
     setInput("");
     setDescription("");
   };
@@ -42,6 +73,7 @@ function TodoForm(props) {
     <form onSubmit={handleSubmit} className="todo-form">
       {props.edit ? (
         <div className="todo-form--update">
+          {/* de aquí abajo es para mostrar unos campos para actualizar */}
           <input
             placeholder="Update your item"
             value={input}
@@ -64,6 +96,7 @@ function TodoForm(props) {
         </div>
       ) : (
         <>
+          {/* Aquí para abajo es para añadir un nuevo to-do */}
           <input
             placeholder="Add a todo"
             value={input}
